52e33c9d70fa6b205c5bdfbd48413b20b72d675e
Maahir Gupta <maahir.gupta@gmail.com>
2021-12-18 13:23:13 -0500

    Updates

---
+++ precache-manifest.536352dd64eaf5a6ab0d2d0947e5b6c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c1ef0aebc2d6276326717bd4e224d9",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "42abb6de91f837a1dfb7",
+    "url": "/website/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "33dfad8ae90fafbb19a3",
+    "url": "/website/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "42abb6de91f837a1dfb7",
+    "url": "/website/static/js/2.0f779a2e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/website/static/js/2.0f779a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33dfad8ae90fafbb19a3",
+    "url": "/website/static/js/main.a2ea2264.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  }
+]);
